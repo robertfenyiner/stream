89b9d7f42efb29feb64aa5b106184ac459c5876c
Robert Niño <75451452+robertfenyiner@users.noreply.github.com>
2025-09-22 16:45:17 -0500

    .

---
+++ src/test/java/com/akshathsaipittala/streamspace/StreamSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertfenyiner.latstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
